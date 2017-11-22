e51fff06bbbc51d3341193821090f8c0832c15dc
oliviergaillard <info@metacoherence.ch>
2017-11-22 16:19:21 +0100

    page d'accueil. Date de fin determinante.

--- homepage/page_processors.py
+++ homepage/page_processors.py
@@ -8,7 +8,7 @@ def news_list_function(request, page):
     """Implicit: there is only one ListNews page."""
     listnews = ListNews.objects.first()
     max_news_on_homepage = listnews.max_news_domo
-    news_list = News.objects.filter(date_debut__gte=datetime.date.today())
+    news_list = News.objects.filter(date_fin__gte=datetime.date.today())
     news_list = news_list[0:max_news_on_homepage]
     ctx = {'news_list' : news_list}
     return ctx
